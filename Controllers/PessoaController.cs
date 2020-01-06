ea2c52af753f328851bcaaf95c1a9fa8221781ff
Luciano Costa <luciano11982@hotmail.com>
2020-01-06 20:36:22 -0300

    Criação da entidade Pessoa

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace APIRest_Contatos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Luciano", "Kelviane" };
+        }
+
+        [HttpGet]
+        public string Get(long id)
+        {
+            return "Value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest_Contatos.Models
+{
+    public class Pessoa
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+        public int Status { get; set; }
+    }
+}
